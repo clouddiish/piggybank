2412e4971e84987751fdc518da0a096a1a68adb2
Olga Szymczak <olga.szymczaak@gmail.com>
2025-07-23 19:18:04 +0200

    fix: import error

--- backend/app/services/role.py
+++ backend/app/services/role.py
@@ -5,7 +5,7 @@ from app.common.enums import EntityType
 from app.core.session import get_session
 from app.db_models import Role
 from app.schemas import RoleCreate, RoleUpdate, RoleFilters
-from backend.app.services.base import BaseService
+from app.services.base import BaseService
 
 
 class RoleService(BaseService[Role, RoleCreate, RoleUpdate, RoleFilters]):

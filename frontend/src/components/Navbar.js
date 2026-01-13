e6013a2e5a445b0b46b00dde09e557dd5f7fdb8d
Olga Szymczak <olga.szymczaak@gmail.com>
2026-01-13 21:03:12 +0100

    feat: add home icon to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,22 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { HiOutlineBars3 } from "react-icons/hi2";
+import { FaPiggyBank } from "react-icons/fa6";
 
 import Menu from "./Menu";
 
 
 const Navbar = () => {
+    const navigate = useNavigate();
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <nav style={{ display: "flex", alignItems: "center", padding: "8px 16px", background: "#1976d2", color: "#fff" }}>
-        <h2 style={{ flex: 1 }}>piggybank</h2>
+        <h2 style={{ flex: 1 }}>
+            <FaPiggyBank style={{ cursor: "pointer" }} size={32} onClick={() => navigate("/transactions")} /> 
+            piggybank
+        </h2>
         <button
             style={{ background: "none", border: "none", color: "#fff", fontSize: "24px", cursor: "pointer" }}
             onClick={() => setMenuOpen((open) => !open)}

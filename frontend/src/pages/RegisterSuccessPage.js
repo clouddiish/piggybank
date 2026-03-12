b55fa6c2cfe592f04b824b606bcf5ed0f3d1e506
Olga Szymczak <olga.szymczaak@gmail.com>
2026-03-12 19:02:02 +0100

    feat: style register success page

--- frontend/src/pages/RegisterSuccessPage.js
+++ frontend/src/pages/RegisterSuccessPage.js
@@ -6,11 +6,20 @@ import Button from "../components/Button";
 const RegisterSuccessPage = () => {
   const navigate = useNavigate();
   return (
-    <>
-      <h1>successfully registered</h1>
-      <p>you can log in now</p>
-      <Button variant="primary" onClick={() => navigate("/login")}>login</Button>
-    </>
+    <div className="mx-md-5 mx-2 py-5 align-items-center vh-100">
+
+      <div className="my-5">
+        <h1>successfully registered</h1>
+        <p>you can log in now</p>
+      </div>
+
+      <div className="row g-3">
+        <div className="col-12 col-sm-auto">
+          <Button variant="primary" className="w-100" onClick={() => navigate("/login")}>login</Button>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
